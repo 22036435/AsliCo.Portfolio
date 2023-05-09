a304da45f55714880ddc7c12e94009a616b4a90e
Ayse Ilhan <a.ilhan0520221@arts.ac.uk>
2023-05-09 21:12:12 +0100

    Update Renderer.js

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
 
-export default class Renderer {
-    constructor() {
+export default class Renderer{
+    constructor(){
         this.experience = new Experience();
         this.sizes = this.experience.sizes;
         this.scene = this.experience.scene;
@@ -10,50 +10,50 @@ export default class Renderer {
         this.camera = this.experience.camera;
 
         this.setRenderer();
-    }
 
-    setRenderer() {
-        this.renderer = new THREE.WebGLRenderer({
+    }
+    
+    setRenderer(){
+        this.renderer= new THREE.WebGL1Renderer({
             canvas: this.canvas,
             antialias: true,
         });
-
-        this.renderer.physicallyCorrectLights = true;
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        
+        this.renderer.useLegacyLights = true;
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace;
         this.renderer.toneMapping = THREE.CineonToneMapping;
-        this.renderer.toneMappingExposure = 1.75;
+        this.renderer.toneMappingExposure = 0.5;
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.renderer.setSize(this.sizes.width, this.sizes.height);
         this.renderer.setPixelRatio(this.sizes.pixelRatio);
     }
 
-    resize() {
+    resize(){
         this.renderer.setSize(this.sizes.width, this.sizes.height);
         this.renderer.setPixelRatio(this.sizes.pixelRatio);
     }
 
-    update() {
-        // this.renderer.setViewport(0, 0, this.sizes.width, this.sizes.height);
+    
+    update(){
+        //this.renderer.setViewport(0,0, this.sizes.width, this.sizes.height);
         this.renderer.render(this.scene, this.camera.orthographicCamera);
-        // Second Screen
-        // this.renderer.setScissorTest(true);
-        // this.renderer.setViewport(
-        //     this.sizes.width - this.sizes.width / 3,
-        //     this.sizes.height - this.sizes.height / 3,
-        //     this.sizes.width / 3,
-        //     this.sizes.height / 3
-        // );
+        //this.renderer.setScissorTest(true);
+        //this.renderer.setViewport(this.sizes.width-this.sizes.width/3,
+        //this.sizes.height-this.sizes.height/3, 
+        //this.sizes.width/ 3, 
+        //this.sizes.height/ 3
+        //);
 
-        // this.renderer.setScissor(
-        //     this.sizes.width - this.sizes.width / 3,
-        //     this.sizes.height - this.sizes.height / 3,
-        //     this.sizes.width / 3,
-        //     this.sizes.height / 3
-        // );
+        //this.renderer.setScissor(this.sizes.width-this.sizes.width/3,
+        //this.sizes.height-this.sizes.height/3, 
+        //this.sizes.width/ 3, 
+        //this.sizes.height/ 3
+        //);
 
-        // this.renderer.render(this.scene, this.camera.perspectiveCamera);
+        //this.renderer.render(this.scene, this.camera.perspectiveCamera);
 
-        // this.renderer.setScissorTest(false);
+        //this.renderer.setScissorTest(false);
+
     }
 }
