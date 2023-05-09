905050348c2a144712139f0e952cc41d08e68dd1
Ayse Ilhan <a.ilhan0520221@arts.ac.uk>
2023-05-09 21:12:25 +0100

    Update Room.js

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -12,9 +12,7 @@ export default class Room{
         this.time = this.experience.time;
         this.room = this.resources.items.room;
         this.actualRoom = this.room.scene;
-        //console.log(this.actualRoom);
         this.roomChildren= {};
-        //console.log(this.roomChildren);
 
         this.lerp={
             current: 0,
@@ -37,11 +35,44 @@ export default class Room{
 
             if(child instanceof THREE.Group){
                 child.children.forEach((groupchild)=>{
-                    //console.log(groupchild.material);
                     groupchild.castShadow = true;
                     groupchild.receiveShadow = true;
                 });
             }
+
+            /*if(child.name === "wallMirror"){
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness = 0;
+                child.material.color.set(0xffffff);
+                child.material.ior= 1;
+                child.material.transmission = 0.43;
+                child.material.opacity = 1;
+                child.material.metalness = 1;
+                child.material.exposure = 1;
+                child.material.envMapIntensity = 1;
+            }
+
+            if(child.name === "mirror"){
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness = 0;
+                child.material.color.set(0xffffff);
+                child.material.ior= 1;
+                child.material.transmission = 0.43;
+                child.material.opacity = 1;
+                child.material.metalness = 1;
+                child.material.exposure = 1;
+                child.material.envMapIntensity = 1;
+            }
+
+            if(child.name === "glassWindow"){
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness = 0;
+                child.material.color.set(0xffffff);
+                child.material.ior= 3;
+                child.material.transmission = 0.43;
+                child.material.opacity = 0.5;
+            }*/
+
             /*if(child.name === "screen1"){
                 child.material = new THREE.MeshBasicMaterial({
                     map: this.resources.items.screen,
@@ -49,33 +80,37 @@ export default class Room{
             }*/
 
             if(child.name === "nfts"){
-                child.children[1].material = new THREE.MeshBasicMaterial({
+                child.children[3].material = new THREE.MeshBasicMaterial({
                     map: this.resources.items.nft1,
                 });
             }
             if(child.name === "nfts"){
-                child.children[2].material = new THREE.MeshBasicMaterial({
+                child.children[1].material = new THREE.MeshBasicMaterial({
                     map: this.resources.items.nft2,
                 });
             }
             if(child.name === "nfts"){
-                child.children[3].material = new THREE.MeshBasicMaterial({
+                child.children[2].material = new THREE.MeshBasicMaterial({
                     map: this.resources.items.nft3,
                 });
             }
 
             if(child.name === "Mini_Floor"){
-                child.position.x = -0.289521;
-                child.position.z= 8.83572;
+                child.position.x = -0.363905;
+                child.position.z= -11.5518;
             }
 
+            //if(child.name === "Mailbox" || 
+            //child.name === "minifloorobjects"){
+                //child.scale.set(0,0,0);
+            //}
 
             child.scale.set(0,0,0);
 
             if(child.name === "initialcube"){
                 //child.scale.set(1,1,1);
-                child.position.set(0, -1, 0);
-                child.rotation.y = Math.PI /4;
+                child.position.set(0, -1.5, 0);
+                //child.rotation.y = Math.PI /4;
             }
 
             this.roomChildren[child.name.toLowerCase()] = child;
@@ -91,7 +126,7 @@ export default class Room{
             width,
             height
         );
-        rectLight.position.set(-0.861942, 13, -2.41421);
+        rectLight.position.set(0.134686, 7, 6.21178);
         rectLight.rotation.x = -Math.PI / 2;
         rectLight.rotation.z = Math.PI / 4;
         this.actualRoom.add(rectLight);
@@ -103,26 +138,27 @@ export default class Room{
 
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11, 0.11, 0.11);
-        //this.actualRoom.rotation.y = Math.PI;
+        this.actualRoom.rotation.y = Math.PI;
     }
 
     setAnimation(){
-        //this.mixer = new THREE.AnimationMixer (this.actualRoom);
+        this.mixer = new THREE.AnimationMixer (this.actualRoom);
         //this.spin = this.mixer.clipAction(this.room.animations[67]);
         //this.spin.play();
     }
 
-    onMouseMove() {
-        window.addEventListener("mousemove", (e) => {
-            this.rotation =
-                ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
-            this.lerp.target = this.rotation * 0.05;
+    onMouseMove(){
+        window.addEventListener("mousemove", (e)=>{
+            this.rotation = (((e.clientX - window.innerWidth/2)*2)/window.innerWidth);
+            this.lerp.target = this.rotation*0.05;
         });
     }
 
-    resize() {}
+    resize(){
+       
+    }
 
-    update() {
+    update(){
         this.lerp.current = GSAP.utils.interpolate(
             this.lerp.current,
             this.lerp.target,
